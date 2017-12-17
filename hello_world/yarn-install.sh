728c783731171f21a91bd8925b0a1b45183434ca
tan90 <tan90indream@163.com>
2017-12-17 18:52:19 +0800

    delete .bat and use yarn

--- hello_world/yarn-install.sh
+++ hello_world/yarn-install.sh
@@ -1,5 +1,5 @@
-cd ./game-server && npm install -d
+cd ./game-server && yarn install
 echo '============   game-server npm installed ============'
 cd ..
-cd ./web-server && npm install -d
+cd ./web-server && yarn install
 echo '============   web-server npm installed ============'
